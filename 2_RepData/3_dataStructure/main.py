cf8a06505e93ece13e53ae004586ecfbb3366455
jxxparker <email for my second account>
2021-07-02 13:00:59 -0500

    do it

---
+++ 2_RepData/3_dataStructure/main.py
@@ -0,0 +1,12 @@
+#abstract interfaces
+
+#collections
+#not all collections encode data the same way. 
+# -is the data ordered or unordered
+# -is the data associatvie, mapping between keys and values?
+# - is the data unique
+
+
+
+
+#mutability and hashability
---
+++ 4_exploreApublicdataset/main.py
@@ -0,0 +1,4 @@
+# Explore a Public Dataset
+# Fortunately, we need not search endlessly for concrete examples of data sets – in this world, we are immersed in them. The inescapable presence of structured information surrounds us and informs all aspects of modern life – not just digital technology, but everything, from earth science to economics, machine learning to museums, language to politics.
+
+# For this exercise, you'll find an existing public dataset and describe its structure, using the language of schemas, in a brief paragraph or two.
