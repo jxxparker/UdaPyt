291e192754a868e5adc36dc701fe454ad4c02f96
jxxparker@gmail.com <email for my second account>
2021-07-06 16:24:30 -0500

    push

--- 2_RepData/11_stringSequences/main.py
+++ 2_RepData/11_stringSequences/main.py
@@ -1,3 +1,4 @@
 #Sequences
 #The first category of collections we examine are sequences – sized, iterable ordered containers of data. We'll see the similarities and differences between three sequence types - list, tuple, and str.
 
+#lets ee
