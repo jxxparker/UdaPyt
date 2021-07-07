ee98e11a9a0c1acd66fe0d9b9e46ec783c0c0d39
jxxparker@gmail.com <email for my second account>
2021-07-07 16:14:40 -0500

    push

--- 2_RepData/11_stringSequences/main.py
+++ 2_RepData/11_stringSequences/main.py
@@ -1,4 +1,24 @@
 #Sequences
-#The first category of collections we examine are sequences – sized, iterable ordered containers of data. We'll see the similarities and differences between three sequence types - list, tuple, and str.
+#The first category of collections we examine are sequences – sized, iterable ordered containers of data. We'll see the similarities and differences between three sequence types - list, tuple, and string
 
-#lets ee
+s = 'Udacity'
+
+# print(s[0])
+# print(s[1])
+# print(s[4])
+# print(s[7]) #wont work
+
+# print(s[-1]) # y
+# print(s[-2])
+
+#--------
+# print(s[0:2]) #Ud from 0 - 2
+# print(s[0:3]) #Uda
+# print(s[4:7]) #ity
+
+print(s[1:5:2]) #dc first number is where we start
+                #   second is where we end
+                #   last number is the jumping number
+
+print(s[4::-2]) #iaU   first number is where we start
+                #       :: means go back
