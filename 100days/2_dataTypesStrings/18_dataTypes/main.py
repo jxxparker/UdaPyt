7fd5f490a34f50ad99825f298ca0c16f9165b232
jxxparker@gmail.com <email for my second account>
2021-07-17 16:04:48 -0500

    push

---
+++ 100days/2_dataTypesStrings/18_dataTypes/main.py
@@ -0,0 +1,12 @@
+# num_char = len(input("whats your name?"))
+
+# new_num_char = str(num_char)
+
+# print(type(num_char))
+# print("your name has " + new_num_char + " characters")
+
+a = 123
+print(type(a))
+
+new_a = str(a)
+print(type(new_a))
