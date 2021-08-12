069b0be58125ffffdcbfdf56d91a1aff533d760e
Jihun Park <jxxparker@gmail.com>
2021-08-12 10:46:00 -0500

    fxninput

---
+++ 100days/8_FxnParameter/79_FxnInputs/main.py
@@ -0,0 +1,11 @@
+def greet():
+    print("Hello")
+    print("How do you do?")
+
+greet()
+
+def greet_with_name(name):
+    print(f"hello {name}")
+    print(f"how do you do {name}")
+
+greet_with_name("jihun")
