c5f2c51c6815ea667227a19606c4885f05c68fb7
Jihun Park <jxxparker@gmail.com>
2021-08-12 10:14:36 -0500

    pushing

--- 100days/7_Hangman/67_hangmanP1/main.py
+++ 100days/7_Hangman/67_hangmanP1/main.py
@@ -10,4 +10,8 @@ chosen_word = random.choice(word_list)
 guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
