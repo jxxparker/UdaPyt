13c82e34a3c6161210887bd7400f874f4bb1b9c9
Jihun Park <jxxparker@gmail.com>
2021-08-15 11:38:28 -0500

    hangman p2

---
+++ 100days/5_pythonLoops/forLoops/main.py
@@ -0,0 +1,19 @@
+a = ["banana", "apple", "microsoft"]
+# print(a[0])
+# print(a[1])
+# print(a[2])
+
+# for element in a:
+#     print(element)
+
+# b = [20, 10, 5]
+# total = 0
+# for element in b:
+#     total += element
+#     print(total)
+
+# list1 = list(range(1,5)) #range from 1-4
+# print(list1)
+
+for i in range(1,5):
+    print(i)
--- 100days/7_Hangman/69_hangmanP2/main2.py
+++ 100days/7_Hangman/69_hangmanP2/main2.py
@@ -25,9 +25,7 @@ guess = input("Guess a letter: ").lower()
 for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-
+        display[position] = letter
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+print(display)
