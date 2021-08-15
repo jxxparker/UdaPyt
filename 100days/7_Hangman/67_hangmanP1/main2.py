7b318ccb08486e8fb7a9787838793b2cf72c3fd8
Jihun Park <jxxparker@gmail.com>
2021-08-15 11:00:37 -0500

    pushing more

--- 100days/7_Hangman/67_hangmanP1/main2.py
+++ 100days/7_Hangman/67_hangmanP1/main2.py
@@ -1,19 +1,17 @@
 #Step 1 
+import random
 
 word_list = ["aardvark"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-import random
 chosen_word = random.choice(word_list)
 
-
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess a letter ").lower()
-
+guess = input("guess a letter please: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
     if letter == guess:
-        print(f"{guess} is a match")
-    else:
-        print(f"{guess} is not a match")
+        print("Correct")
+    else: 
+        print("Wrong")
--- 100days/7_Hangman/70_p2sol/main.py
+++ 100days/7_Hangman/70_p2sol/main.py
@@ -1,7 +1,6 @@
 #Step 2
-
 import random
-word_list = ["jihun"]
+word_list = ["aardvark"]
 chosen_word = random.choice(word_list)
 
 #Testing code
@@ -12,8 +11,10 @@ print(f'Pssst, the solution is {chosen_word}.')
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
 word_length = len(chosen_word)
+
 for _ in range(word_length):
     display += "_"
+print(display)
 
 guess = input("Guess a letter: ").lower()
 
@@ -22,11 +23,10 @@ guess = input("Guess a letter: ").lower()
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 for position in range(word_length):
     letter = chosen_word[position]
-
     if letter == guess:
         display[position] = letter
+    
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.s
-
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 print(display)
