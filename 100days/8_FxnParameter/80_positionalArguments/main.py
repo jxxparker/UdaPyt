b5f634aa89343b9c89b5c45deb28451565387158
Jihun Park <jxxparker@gmail.com>
2021-08-15 18:35:34 -0500

    fxn

--- 100days/8_FxnParameter/80_positionalArguments/main.py
+++ 100days/8_FxnParameter/80_positionalArguments/main.py
@@ -1,1 +1,11 @@
+#super simple fxn
 def greet():
+    print("hello ghoney")
+
+greet()
+
+#fxn that allows for input
+def greet_with_name(name):
+    print(f"hello {name}")
+
+greet_with_name("jihun")
