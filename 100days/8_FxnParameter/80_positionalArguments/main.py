30541bb4c1e2d642a5ab61e21bf16737ac4f9981
Jihun Park <jxxparker@gmail.com>
2021-08-15 16:51:32 -0500

    pushing more

--- 100days/8_FxnParameter/80_positionalArguments/main.py
+++ 100days/8_FxnParameter/80_positionalArguments/main.py
@@ -1,18 +1,1 @@
-# def greet():
-#     print("Hello")
-#     print("How do you do?")
-
-# greet()
-
-# def greet_with_name(name):
-#     print(f"hello {name}")
-#     print(f"how do you do {name}")
-
-# greet_with_name("jihun")
-
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"what is it like in {location}")
-
-greet_with("jihun", "california")
-
+def greet():
