4f9e4ff0c4fcf50c97c8dcde259628945401536a
Jihun Park <jxxparker@gmail.com>
2021-08-24 15:40:08 -0500

    callculator

--- 100days/10_fxnOutputs/101_calcP1/main.py
+++ 100days/10_fxnOutputs/101_calcP1/main.py
@@ -25,3 +25,10 @@ num2 = int(input("what is the second number: "))
 
 for symbol in operations:
     print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
