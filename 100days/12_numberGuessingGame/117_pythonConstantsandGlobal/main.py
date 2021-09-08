10ab8fba3bf435a22ad266c93046e163ff3d0266
Jihun Park <jxxparker@gmail.com>
2021-09-08 16:14:41 -0500

    pushing numbers game

---
+++ 100days/12_numberGuessingGame/117_pythonConstantsandGlobal/main.py
@@ -0,0 +1,6 @@
+#global constants
+
+PI =  3.14
+URL = "www.google.com"
+TWITTER = "@GHONEY"
+
---
+++ 100days/12_numberGuessingGame/quiz_/main.py
@@ -0,0 +1,31 @@
+#question 1
+#what will be printed in the console when the following code is run?
+
+def a_function(a_parameter):
+    a_variable = 15
+    return a_parameter
+ 
+a_function(10)
+print(a_variable)
+
+#question 2
+#what will be printed in the console when the code is run?
+i = 50
+def foo():
+    i = 100
+    return i
+ 
+foo()
+print(i)
+
+#question 3
+#what will be printed in the console when the following code is run?
+def bar():
+    my_variable = 9
+ 
+    if 16 > 9:
+      my_variable = 16
+ 
+    print(my_variable)
+ 
+bar()
