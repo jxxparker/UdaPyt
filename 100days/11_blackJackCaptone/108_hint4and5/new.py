650b629d1e79d875501246282fe965997fb53d84
Jihun Park <jxxparker@gmail.com>
2021-09-08 09:47:14 -0500

    pushing blackjack re

--- 100days/11_blackJackCaptone/108_hint4and5/new.py
+++ 100days/11_blackJackCaptone/108_hint4and5/new.py
@@ -1,26 +1,27 @@
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 import random
 
+##################### Hints #####################
+
+#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
+#11 is the Ace.
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    random_card = random.choice(cards)
-    return random_card
+    card = random.choice(cards)
+    return card
 
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
 computer_cards = []
 
-for _ in range(2):
+for run in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
+    
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
-def calculate_score(set_cards):
-    #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-    if sum(set_cards) == 21 and len(set_cards) == 2:
-        return 0 #black jack completed
-
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
