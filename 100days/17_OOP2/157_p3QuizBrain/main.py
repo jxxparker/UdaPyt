1daecbf3b1d3bdcd973ed4cc3a0b49d4e2115c5f
Jihun Park <jxxparker@gmail.com>
2021-10-05 17:15:07 -0500

    puishing

---
+++ 100days/17_OOP2/157_p3QuizBrain/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+#contain a question bank and answer
+for question in question_data:
+    question_text = question["text"] #this will initialize the text from questioon_data in data.py
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+    
