60e05bf9c6759a64f4d3f904445415e9d2f8ce93
Jihun Park <jxxparker@gmail.com>
2021-10-05 17:59:52 -0500

    forloop

--- 100days/5_pythonLoops/48_loopWithpythonLists/main.py
+++ 100days/5_pythonLoops/48_loopWithpythonLists/main.py
@@ -1,6 +1,6 @@
 fruits = ["apple", "peach", "pear"]
 
 for fruit in fruits:
-    print(fruit)
-    print(fruit + " pie")
-print(fruits)
+    # print(fruit)
+    # print(fruit + " Pie") 
+    print(fruits)
