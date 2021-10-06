51b30209d41ab5107b808bf704b8865b9898d6a8
Jihun Park <jxxparker@gmail.com>
2021-10-06 16:39:29 -0500

    adding even

--- 100days/5_pythonLoops/52_addingEvenNumbers/main.py
+++ 100days/5_pythonLoops/52_addingEvenNumbers/main.py
@@ -1,12 +1,6 @@
 total = 0
-for number in range(2, 101,2):
-    total += number
-print(total)    
+for numbers in range (1, 101):
+    if numbers % 2 == 0:
+        total += numbers
 
-
-total2 = 0
-for number in range(1, 101):
-    if number % 2 == 0:
-        total2 += number
-
-print(total2)
+print(total)
