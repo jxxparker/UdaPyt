6debc0e1b5566fca714eb16477e1754d50e5223a
Jihun Park <jxxparker@gmail.com>
2021-10-05 23:16:13 -0500

    push

--- 100days/5_pythonLoops/50_highScore/main.py
+++ 100days/5_pythonLoops/50_highScore/main.py
@@ -3,11 +3,11 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-
 highest_score = 0
 for score in student_scores:
-    if score > highest_score: #checks if current is higher than before
-        highest_score = score   #eventually will stop when highest is set
-print(f"the highest score is {highest_score}")
-    
+      if score > highest_score:
+            highest_score = score
+            print(highest_score)
+
+print(f"The highest score in the class is: {highest_score}")
 
