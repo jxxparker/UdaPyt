daab13dd486e8d218a4ea46a8267604eefc48935
Jihun Park <jxxparker@gmail.com>
2021-10-06 23:03:20 -0500

    pushing hang man p1

--- 100days/5_pythonLoops/54_createPassword/hardpassword.py
+++ 100days/5_pythonLoops/54_createPassword/hardpassword.py
@@ -9,22 +9,23 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password_list = []
+password = []
 
-for char in range(1, nr_letters + 1):
-    password_list += random.choice(letters)
+for words in range(1, nr_letters + 1):
+    password.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
-    password_list += random.choice(symbols)
+for words in range(1, nr_symbols + 1):
+    password.append(random.choice(symbols))
 
-for char in range(1, nr_numbers + 1):
-    password_list += random.choice(numbers)
+for words in range(1, nr_numbers + 1):
+    password.append(random.choice(numbers))
 
-random.shuffle(password_list)
-print(password_list)
-
-password = ""
-for char in password_list:
-    password += char
-
+random.shuffle(password)
 print(password)
+
+
+actual_password = ""
+for char in password:
+    actual_password += char
+
+print(actual_password)
--- 100days/7_Hangman/67_hangmanP1/main1.py
+++ 100days/7_Hangman/67_hangmanP1/main1.py
@@ -1,17 +1,18 @@
 #Step 1 
-import random
 
-word_list = ["aardvark"]
+word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
 chosen_word = random.choice(word_list)
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess a letter please: ").lower()
+guess = input("guess a letter ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
     if letter == guess:
-        print("Correct")
-    else: 
-        print("Wrong")
+        print(f"{guess} is a match")
+    else:
+        print(f"{guess} is not a match")
+        
