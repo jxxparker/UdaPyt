b9aff0c233cc480fc9289b0494316fbea5e17e4b
Jihun Park <jxxparker@gmail.com>
2021-11-01 10:53:36 -0500

    pushing

--- 100days/12_numberGuessingGame/115_pythonScope/main.py
+++ 100days/12_numberGuessingGame/115_pythonScope/main.py
@@ -1,14 +1,13 @@
-#there is no block scope
+#there is no block scope in python
 
 # if 3 > 2:
 #     a_variable = 10
 
-game_level =3
-
+game_level = 3   
 def create_enemy():
-    enemies = ["skeleon", "zombie", "alien"]
+    enemies = ["skeleton", "zombie", "alien"]
 
     if game_level < 5:
-        new_enemies = enemies[0]
+        new_enemy = enemies[0]
 
-    print(new_enemies)
+    print(new_enemy)
