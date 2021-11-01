d721c9fa784cd140c04afbf945a2573ed509623c
Jihun Park <jxxparker@gmail.com>
2021-11-01 10:59:55 -0500

    constant and global scope

--- 100days/12_numberGuessingGame/116_modifyGlobalVariable/main.py
+++ 100days/12_numberGuessingGame/116_modifyGlobalVariable/main.py
@@ -1,14 +1,13 @@
+#Modifying global scope
+
 enemies = 1
 
-# def increase_enemies():
-#     global enemies #this tells that theres enemies outside of the function
-#     enemies += 1
-#     print(f"enemies inside function: {enemies}")
-
 def increase_enemies():
+    # global enemies #telling global variable outside of function
+    # enemies += 1
     print(f"enemies inside function: {enemies}")
     return enemies + 1
 
-enemies = increase_enemies()
-increase_enemies()
-print(f"enemies outside function: {enemies}")
+increase_enemies() #output 2
+print(f"enemies outside function: {enemies}") #output 1 
+
