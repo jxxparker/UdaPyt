3a9710ec307cd0d88ad87df0d89adaf8ec71fe5f
Jihun Park <jxxparker@gmail.com>
2021-11-16 12:45:13 -0600

    almost complete quiz

--- 100days/17_OOP2/160_p6checkingAnswers/quiz_brain.py
+++ 100days/17_OOP2/160_p6checkingAnswers/quiz_brain.py
@@ -8,6 +8,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         # if self.question_number < len(self.question_list):
@@ -21,6 +22,22 @@ class QuizBrain:
     #use the input() fxn to show the user the Questin text and ask for the user's answer.
 
     def next_question(self):
-        current_question = self.question_list[0]
+        current_question = self.question_list[self.question_number]
+        correct_answer = current_question.answer
+        
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False) ")
+        
+        self.check_answer(user_answer, correct_answer) 
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("Right")
+        else:
+            print(f"Wrong")
+        print(f"  Correct Answer: {correct_answer}")
+        print(f"  Current Score: {self.score}/{self.question_number}")
+
+    
+        
