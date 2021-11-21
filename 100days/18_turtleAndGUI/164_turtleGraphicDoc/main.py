d090672ca85fca46a76ea3f6e5cb145bc6b83a4f
Jihun Park <jxxparker@gmail.com>
2021-11-21 00:03:29 -0600

    push

--- 100days/18_turtleAndGUI/164_turtleGraphicDoc/main.py
+++ 100days/18_turtleAndGUI/164_turtleGraphicDoc/main.py
@@ -4,15 +4,7 @@ timmy_the_turtle = Turtle() #creating timmy the turtle from turtle
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
 timmy_the_turtle.forward(100)
-
-
-
-
-
-
-
- 
-
+timmy_the_turtle.right(90)
 
 
 
---
+++ 100days/18_turtleAndGUI/165_turtleChallenge/main.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+tim = Turtle() #creating timmy the turtle from turtle
+
+
+for _ in range(4):
+    tim(100)
+    tim(90)
+
+
+    
+
+    
+screen = Screen() #doesn't let screen exit, because without it it does.
+screen.exitonclick() 
