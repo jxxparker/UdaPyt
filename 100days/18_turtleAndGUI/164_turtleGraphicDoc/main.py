87b1489567fc4dc214e0a0c726b9249b62550270
Jihun Park <jxxparker@gmail.com>
2021-11-20 23:36:57 -0600

    commit

---
+++ 100days/18_turtleAndGUI/164_turtleGraphicDoc/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle() #creating timmy the turtle from turtle
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+screen = Screen() #doesn't let screen exit, because without it it does.
+screen.exitonclick() 
