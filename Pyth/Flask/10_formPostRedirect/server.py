1aa6df590c4a5c7108f3a129f30ea0a3a267b3eb
Jihun Park <jxxparker@gmail.com>
2021-12-20 00:42:03 -0600

    re pushing formpostrequest

--- Pyth/Flask/10_formPostRedirect/server.py
+++ Pyth/Flask/10_formPostRedirect/server.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, session, request, redirect
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    print("Got Post Info")
-    
+@app.route("/users", methods=['POST'])
+def create():
+    print(request.form)
+    print('Name', request.form['name'])
+    print('Email', request.form['email'])
     name = request.form['name']
     email = request.form['email']
-    
-    return redirect('/')
+    return render_template("create.html")
+
 if __name__=="__main__":
-    
-    app.run(debug=True) 
+    app.run(host="localhost", port=8000, debug=True)
