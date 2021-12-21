f0817d08648da7ab2d6c60f0389a19adc748e428
Jihun Park <jxxparker@gmail.com>
2021-12-21 14:27:51 -0600

    list comprehension basics

---
+++ 100days/26_natoAlphabet/234_createLists/main.py
@@ -0,0 +1,20 @@
+#list comprehension
+
+# numbers = [1,2,3]
+# new_numbers = [n + 1 for n in numbers]
+# print(new_numbers)
+
+# name = "ghoney"
+# new_list = [letter for letter in name]
+# print(new_list)
+
+# range_list = [num * 2 for num in range(1,5)]
+# print(range_list)
+
+names = ["ghoney", "Jordan", "James", "Bryant", "Jihun", "Kevin"]
+short_names = [name for name in names if len(name) < 6]
+print(short_names)
+
+# --------------
+upper_names = [name.upper() for name in names] 
+print(upper_names)
