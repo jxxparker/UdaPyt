aad9bbe3195407c654d0050e34e1338d448798e3
Jihun Park <jxxparker@gmail.com>
2021-12-21 14:58:49 -0600

    squared numbers using list compre

--- 100days/26_natoAlphabet/234_createLists/main.py
+++ 100days/26_natoAlphabet/234_createLists/main.py
@@ -1,15 +1,15 @@
 #list comprehension
 
-# numbers = [1,2,3]
-# new_numbers = [n + 1 for n in numbers]
-# print(new_numbers)
+numbers = [1,2,3]
+new_numbers = [n + 1 for n in numbers]
+print(new_numbers)
 
-# name = "ghoney"
-# new_list = [letter for letter in name]
-# print(new_list)
+name = "ghoney"
+new_list = [letter for letter in name]
+print(new_list)
 
-# range_list = [num * 2 for num in range(1,5)]
-# print(range_list)
+range_list = [num * 2 for num in range(1,5)]
+print(range_list)
 
 names = ["ghoney", "Jordan", "James", "Bryant", "Jihun", "Kevin"]
 short_names = [name for name in names if len(name) < 6]
