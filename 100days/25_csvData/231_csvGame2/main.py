bc2c6a0fc3e3f9f75367b8c1485c2d0604a95922
Jihun Park <jxxparker@gmail.com>
2021-12-21 12:46:29 -0600

    csv states game

--- 100days/25_csvData/231_csvGame2/main.py
+++ 100days/25_csvData/231_csvGame2/main.py
@@ -9,21 +9,25 @@ turtle.shape(image)
 
 data = pandas.read_csv('/Users/jihunpark/Desktop/UdaPyt/100days/25_csvData/231_csvGame2/50_states.csv')
 all_states = data.state.to_list() #to_list turns data.state into a list
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name")
-print(answer_state)
+while len(guessed_states) < 50:
 
-#if answer_state is one of the states in all the states of the 50+states.csv
-    #if they got it right:
-        #create a turtle to write the name of the state at the state's x and y coordinate
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name").title()
 
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(answer_state)
+    
+
+    #if answer_state is one of the states in all the states of the 50+states.csv
+        #if they got it right:
+            #create a turtle to write the name of the state at the state's x and y coordinate
+    if answer_state in all_states:
+        guessed_states.append(answer_state) 
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
 
 
 screen.exitonclick() 
---
+++ 100days/25_csvData/232_csvGame3/main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "/Users/jihunpark/Desktop/UdaPyt/100days/25_csvData/230_cvsGame/states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv('/Users/jihunpark/Desktop/UdaPyt/100days/25_csvData/231_csvGame2/50_states.csv')
+all_states = data.state.to_list() #to_list turns data.state into a list
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_missing.csv")
+        break
+
+    #if answer_state is one of the states in all the states of the 50+states.csv
+        #if they got it right:
+            #create a turtle to write the name of the state at the state's x and y coordinate
+    if answer_state in all_states:
+        guessed_states.append(answer_state) 
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+# screen.exitonclick() 
