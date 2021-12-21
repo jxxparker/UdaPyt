fb83348f1080ec94d3e2bbd7516f99f07f13ec82
Jihun Park <jxxparker@gmail.com>
2021-12-21 16:27:05 -0600

    counter

---
+++ Pyth/FlaskProj/secondHalf/13_counter/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, render_template, request, session
+app = Flask(__name__)
+app.secret_key = "MySecretKey@!"
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        session["counter"] = 0
+        return render_template('index.html')
+    elif request.method == 'POST':
+        inc_value = request.form['button']
+        if inc_value == '+1':
+            session['counter'] += 1
+        elif inc_value == '+2':
+            session['counter'] += 2
+        elif inc_value == 'reset':
+            return redirect('/')
+        else:
+            return "ERROR"
+        return redirect('/success')
+    else:
+        return "ERROR"
+    return redirect('/')
+
+@app.route('/success')
+def success():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(host="localhost", port=8000, debug=True)
