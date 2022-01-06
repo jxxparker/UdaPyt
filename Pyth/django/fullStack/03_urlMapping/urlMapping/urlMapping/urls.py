e05a8750e86d9007523a214c9dc213f90c69060d
Jihun Park <jxxparker@gmail.com>
2022-01-06 12:06:09 -0600

    pushing some djagno stuff

--- Pyth/django/fullStack/03_urlMapping/urlMapping/urlMapping/urls.py
+++ Pyth/django/fullStack/03_urlMapping/urlMapping/urlMapping/urls.py
@@ -1,4 +1,4 @@
-"""cloneProject URL Configuration
+"""urlMapping URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
-from cloneproj_app import views
+from first_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index,name='index'),
+    url(r'^first_app/', include('first_app.urls')),
 ]
