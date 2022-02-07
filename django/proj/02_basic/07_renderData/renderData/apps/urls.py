3411f694a799a8558dfbb9e686ad371441341278
Jihun Park <jxxparker@gmail.com>
2022-02-07 13:43:52 -0600

    pushing

--- django/proj/02_basic/07_renderData/renderData/apps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from apps import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('index2/<str:pk>', views.index2, name="index2"), 
-    
-    
-]
--- django/proj/02_basic/07_renderingData/dev/dev/settings.py
+++ django/proj/02_basic/07_renderingData/dev/dev/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 import os
 
@@ -10,12 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sa@8p5)6r0m=-*58juulqnzarx20dby(hev0o(#3ck=96j&ye8'
+SECRET_KEY = 'django-insecure-)ot829$up&ex$pq^kgh+jymc7j7@mh7ba2#34ww$=c#&x9j4&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['jihun-portfolio.herokuapp.com', 'www.jihunypark.com']
 ALLOWED_HOSTS = []
 
 
@@ -28,11 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps',
+    'apps.apps.AppsConfig'
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,13 +39,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'portfolio.urls'
+ROOT_URLCONF = 'dev.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates'), 
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -62,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio.wsgi.application'
+WSGI_APPLICATION = 'dev.wsgi.application'
 
 
 # Database
@@ -107,18 +104,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
