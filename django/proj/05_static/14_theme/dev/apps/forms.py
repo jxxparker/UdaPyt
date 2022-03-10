cc135bd59bdde97c03f0acf486fd78286cf8b13e
Jihun Park <jxxparker@gmail.com>
2022-03-10 12:17:47 -0600

    styling finished

--- django/proj/05_static/14_theme/dev/apps/forms.py
+++ django/proj/05_static/14_theme/dev/apps/forms.py
@@ -1,10 +1,19 @@
-#this page makes crud page show 
-
-
 from django.forms import ModelForm 
 from .models import Project
+from django import forms 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ["title", "featured_image", "description", "demo_link", "tags"]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        
+        # self.fields["title"].widget.attrs.update({"class": "input", "placeholder": "Add title"})
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({"class": "input"})
