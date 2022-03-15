8d368225504ea6b2b57826853eea48fe527710d7
Jihun Park <jxxparker@gmail.com>
2022-03-15 15:43:52 -0500

    pushign 9

---
+++ django/proj/03_buildingDatabase/09_databaseRelationships/redo/apps/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+projectsList = [
+    {
+        "id": "1",
+        "title": "Eccomerce Website",
+        "description": "Fully functional ecomerce website"
+    },
+    {
+        "id": "2",
+        "title": "Portfolio Website",
+        "description": "This was project where I built out for my portoflio"
+    },
+    {
+        "id": "3",
+        "title": "Social Network",
+        "description": "Awesome open source projects that is being worked on."
+    },
+]
+
+
+def index(request):
+    page = "projects"
+    msg = "Hello, you are on index page."
+    number = 10
+    context = {"page" : page, "number" : number, "projectsList": projectsList}
+    return render(request, "apps/index.html", context)
+
+def display(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i["id"] == pk:
+            projectObj = i
+    return render(request, "apps/display.html", {"projectsList": projectObj})
