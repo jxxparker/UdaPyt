f92981ab37f7123979448609f14275fbd74dd9dd
Jihun Park <jxxparker@gmail.com>
2022-06-06 16:53:15 -0500

    pushing return value

--- Javascript/02_basics/27_customFxn/dev/assets/scripts/app.js
+++ Javascript/02_basics/27_customFxn/dev/assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert("The result is " + result);
+}
 
+add(1, 2);
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
 outputResult(currentResult, calculationDescription);
+
