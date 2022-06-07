9830926c0fe128e138d26fd2fd70378b5dbf2d8d
Jihun Park <jxxparker@gmail.com>
2022-06-07 15:23:21 -0500

    pushing operator objects

--- Javascript/02_basics/23_/dev/assets/scripts/app.js
+++
@@ -1,6 +0,0 @@
-let currentResult = 0;
-
-currentResult = currentResult + 10 * 3;
-let calculationDescription = '(' + currentResult + '(4 * 10)';
-
-outputResult(currentResult, calculationDescription);
--- Javascript/02_basics/24_/dev/assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
-
-currentResult = (currentResult + 10) * 3;
-
-let calculationDescription = '(' + defaultResult + '(4 * 10)';
-
-outputResult(currentResult, calculationDescription);
