5ed9802337da08a9a503c6b23963411725189b7c
Jihun Park <jxxparker@gmail.com>
2022-06-08 23:26:02 -0500

    pushing defer and async notes

--- Javascript/02_basics/calculator/dev/assets/scripts/app.js
+++ Javascript/02_basics/calculator/dev/assets/scripts/app.js
@@ -51,6 +51,7 @@ function multiply() {
   currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
   writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult)
+  
 }
 
 
