6dfefc814ec00c9e4b9166bdf12d9e48c506623a
Jihun Park <jxxparker@gmail.com>
2022-06-16 17:41:25 -0500

    pushing

---
+++ Python/day2/20.py
@@ -0,0 +1,8 @@
+#Type Error, Type Checking and Type cConversion
+
+num_char = len(input("your name?"))
+new_num_char = str(num_char)
+print("your names char count has " + new_num_char)
+
+a = str(123)
+print(type(a))
