87115ea41f49ced9bdee3f58d1bc4e2f5a5aabff
Jihun Park <jxxparker@gmail.com>
2022-06-21 23:33:54 -0500

    push

---
+++ Javascript/06_Function/126-/app.js
@@ -0,0 +1,18 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is starting....')
+}
+
+// // const person = {name: 'Ghoney'};
+// // console.log(person.name);
+
+// const person = {
+//     name: 'ghoney',
+//     greet: function greet() {
+//         console.log('Hellooooo there');
+//     }
+// };
+
+person.greet();
+startGameBtn.addEventListener('click', startGame);
--- Python/day2/20.py
+++
@@ -1,8 +0,0 @@
-#Type Error, Type Checking and Type cConversion
-
-num_char = len(input("your name?"))
-new_num_char = str(num_char)
-print("your names char count has " + new_num_char)
-
-a = str(123)
-print(type(a))
