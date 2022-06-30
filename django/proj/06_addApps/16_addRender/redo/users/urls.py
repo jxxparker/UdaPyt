6b5a4922b4327bc1f978d89d37f36458ef6dec1c
Jihun Park <jxxparker@gmail.com>
2022-06-30 00:28:29 -0500

    push

--- django/proj/06_addApps/16_addRender/redo/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from users import views
-
-urlpatterns = [
-    path("", views.profiles, name="profiles"),
-    path("userProfile/<str:pk>", views.userProfile, name="userProfile"),
-]
