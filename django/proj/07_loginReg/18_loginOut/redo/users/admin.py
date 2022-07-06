279ca215b95c10450c69211885eb599af401c9b4
Jihun Park <jxxparker@gmail.com>
2022-07-06 00:59:57 -0500

    pushing login auth

--- django/proj/07_loginReg/18_loginOut/redo/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Profile, Skill
-
-admin.site.register(Profile) #registers profile on admin
-admin.site.register(Skill) #registers profile on admin
