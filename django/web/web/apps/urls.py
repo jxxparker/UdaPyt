f54e57fb42cd239ce78c01e3e4ccbb284cca1de1
Jihun Park <jxxparker@gmail.com>
2022-07-12 16:14:46 -0500

    pushing editing skills

--- django/web/web/apps/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from apps import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('contactme/', views.contactme, name="contactme"),
-    path('aboutme/', views.aboutme, name="aboutme"),
-    path('projects/', views.projects, name="projects"),
-]
