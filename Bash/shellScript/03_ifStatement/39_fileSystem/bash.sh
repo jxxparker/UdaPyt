568b9f50f74065267078f78397c0f3d9f7e3a018
Jihun Park <jxxparker@gmail.com>
2022-08-05 16:01:25 -0500

    pushing django serializer

--- Bash/shellScript/03_ifStatement/39_fileSystem/bash.sh
+++ Bash/shellScript/03_ifStatement/39_fileSystem/bash.sh
@@ -1,3 +1,54 @@
 #!/bin/bash
 
-# for provided file print the summary of what permissions 
+# for provided file print the summary of what permissions user has granted
+# example: ./filetest.sh hello.txt
+# READ : YES
+# WRITE : NO
+# EXECUTE : NO
+
+# ARGUMENT CHECK
+if [ $# -ne 1 ]; then
+    echo "provide exactly one argument"
+    exit 1
+fi
+
+#variable assignment
+FILE=$1
+# check if the file exists
+if [ -f $FILE ]; then
+
+    #default variables
+    VAR_READ="NO"
+    VAR_WRITE="NO"
+    VAR_EXT=""
+
+    #CHECK IF FILE IS READABLE
+    if [ -r $FILE ]; then
+        VAR_READ="YES"
+    fi
+
+    # check if file is writable
+    if [ -w $FILE ]; then
+        VAR_WRITE="YES"
+    fi
+
+    # check if file is executable
+    if [ -x $FILE ]; then
+        VAR_EXE="YES"
+    fi
+
+    #write permission summary to user
+    echo "READ: $VAR_READ"
+
+
+
+
+
+
+
+
+
+
+
+
+
